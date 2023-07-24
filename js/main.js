aafb77f00a14a2920aa5b2fae65dd35a6b50ed02
Matias Bertuccio <tutebertuccio@hotmail.com>
2023-07-24 11:24:33 -0300

    Agrega iluminación a los cuadros de la maquina y se cambia la opacidad
    Es creado para que el usuario pueda ir viendo y memorizando los cuadros generados de forma aleatoria

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const secuenciaMaquina = [];
+let retrasoMaquina = 0;
+
 const $botonJugar = document.querySelector("#boton-jugar");
 $botonJugar.onclick = comenzarJuego;
 
@@ -8,4 +11,21 @@ function comenzarJuego() {
 function turnoMaquina() {
     const $cuadros = document.querySelectorAll(".cuadro");
     const $cuadroMaquina = $cuadros[Math.floor(Math.random() * $cuadros.length)];
+    secuenciaMaquina.push($cuadroMaquina);
+
+    for (let i = 0; i < secuenciaMaquina.length; i++) {
+        setTimeout(() => {
+            iluminarCuadro(secuenciaMaquina[i]);
+        }, retrasoMaquina);
+
+        retrasoMaquina += 1000;
+    }
+}
+
+function iluminarCuadro($cuadro) {
+    $cuadro.style.opacity = 0.5;
+
+    setTimeout(() => {
+        $cuadro.style.opacity = 1;
+    }, 500);
 }
